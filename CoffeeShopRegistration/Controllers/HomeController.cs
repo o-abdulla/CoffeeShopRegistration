7785752165e42fc8358dde572957a110a8915edc
Omar Abdulla <osabdulla@gmail.com>
2023-08-09 17:15:20 -0400

    First

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "CoffeeShop";
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result(User u)
+        {
+            return View(u);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
